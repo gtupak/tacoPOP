c22476c2be89055e0a87f0731e93981ee4606171
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-26 15:30:14 -0400

    Items now shake on click.

---
+++ TacoPOP/Protocols/Shakeable.swift
@@ -0,0 +1,23 @@
+//
+//  Shakeable.swift
+//  TacoPOP
+//
+//  Created by Gabriel T on 2017-09-26.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import UIKit
+
+protocol Shakeable {}
+
+extension Shakeable where Self: UIView {
+    func shake() {
+        let anim = CABasicAnimation(keyPath: "position")
+        anim.duration = 0.05
+        anim.repeatCount = 5
+        anim.autoreverses = true
+        anim.fromValue = NSValue(cgPoint: CGPoint(x: self.center.x, y: self.center.y))
+        anim.toValue  = NSValue(cgPoint: CGPoint(x: self.center.x + 4.0, y: self.center.y))
+        layer.add(anim, forKey: "position")
+    }
+}
