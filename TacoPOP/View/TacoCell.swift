1f9d332956db3f85fe11f7bf50da09621a573e95
Gabriel T <gabriel.tapuc@gmail.com>
2017-09-26 13:30:19 -0400

    Added the custom TacoCell view.

---
+++ TacoPOP/View/TacoCell.swift
@@ -0,0 +1,24 @@
+//
+//  TacoCell.swift
+//  TacoPOP
+//
+//  Created by Gabriel T on 2017-09-26.
+//  Copyright © 2017 Gabriel T. All rights reserved.
+//
+
+import UIKit
+
+class TacoCell: UICollectionViewCell {
+
+    @IBOutlet weak var tacoImage: UIImageView!
+    @IBOutlet weak var tacoLabel: UILabel!
+    
+    var taco: Taco!
+    
+    func configureCell(taco: Taco) {
+        self.taco = taco
+        tacoImage.image = UIImage(named: taco.proteinID.rawValue)
+        tacoLabel.text = taco.productName
+    }
+
+}
